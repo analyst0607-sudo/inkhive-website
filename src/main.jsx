30e6957a2fdd514135a638dca53b0573a09d6701
analyst0607-sudo <analyst0607@gmail.com>
2026-04-27 23:20:04 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import InkHiveWebsite from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <InkHiveWebsite />
+  </React.StrictMode>
+);
